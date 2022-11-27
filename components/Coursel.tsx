77e6cfc077fe2bdb594f93d6e594bcb9733324c1
Julio Rojas <juliorojas81871@gmail.com>
2022-11-26 20:50:07 -0500

    first commit

---
+++ components/Coursel.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
+import Image from "next/image";
+
+function Coursel() {
+  return (
+    <div className="relative">
+      <div className="absolute h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img
+            src="/carousel1.webp"
+            loading="lazy"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="carousel2.webp"
+            loading="lazy"
+          />
+        </div>
+        <div className="flex justify-around items-center bg-gray-100  text-left">
+          <div className="flex items-center flex-col justify-center bg-gray-100">
+            <h1>Soccer eupnoria is here</h1>
+            <p>Show your passion and take amazing collectibles with you</p>
+            <button className="bg-blue-500 px-2 rounded-sm shadow-md text-white hover:bg-white hover:text-blue-500">
+              Go Now! <ArrowForwardOutlinedIcon />
+            </button>
+          </div>
+          <div className="flex overflow-hidden mr-0 max-w-6xl">
+            <Image
+              width={200}
+              height={200}
+              objectFit="contain"
+              className="w-[100px] object-contain"
+              src="/carousel3-1.webp"
+              alt="first"
+              loading="lazy"
+            />
+            <Image
+              width={200}
+              height={200}
+              objectFit="contain"
+              src="/carousel3-2.webp"
+              alt="second"
+              loading="lazy"
+            />
+            <Image
+              width={200}
+              height={200}
+              objectFit="contain"
+              src="/carousel3-3.webp"
+              alt="third"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Coursel;
---
+++ components/Header.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
+import LanguageIcon from "@mui/icons-material/Language";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import Link from "next/link";
+import Image from "next/image";
+
+type Props = {};
+
+function Header({}: Props) {
+  const connectWithMetamask = useMetamask();
+  const disconnectWithMetamask = useDisconnect();
+  const address = useAddress();
+  return (
+    <div className="max-w-6xl mx-auto p-2">
+      <div className="sm:hidden p-2 flex justify-center">
+        <Link href="/">
+          <Image
+            alt="THirdweb ebaylogo"
+            width={80}
+            height={80}
+            src="/ebay-logo.png"
+            className=" h-full w-full object-contain"
+          />
+        </Link>
+      </div>
+      <nav className="flex justify-between">
+        <div className="flex items-center space-x-4 ">
+          {address ? (
+            <button
+              onClick={disconnectWithMetamask}
+              className="connectWalletbtn text-xs"
+            >
+              Hi, {address.slice(0, 5) + "..." + address.slice(-4)}
+            </button>
+          ) : (
+            <button
+              onClick={connectWithMetamask}
+              className="connectWalletbtn text-xs"
+            >
+              Connect To Your wallet{" "}
+            </button>
+          )}
+
+          <p className="headerLinks">eBay Offers</p>
+          <p className="headerLinks">Help & Contact</p>
+        </div>
+        <div className="flex items-center text-sm space-x-4">
+          <LanguageIcon className="headerLinks" />
+          <p className="headerLinks">Ship to</p>
+          <p className="headerLinks"> Sell</p>
+          <p className="headerLinks">Watchlist</p>
+          <Link className="flex items-center hover:link" href="/addItem">
+            {" "}
+            Add to inventory <ExpandMoreIcon />
+          </Link>
+          <p className="headerLinks">
+            {" "}
+            My eBay{" "}
+            <span>
+              <ExpandMoreIcon />
+            </span>
+          </p>
+          <NotificationsNoneIcon />
+          <ShoppingCartOutlinedIcon />
+          <p className=""></p>
+        </div>
+      </nav>
+      <hr className="mt-2" />
+
+      <section className="flex items-center space-x-2 py-5">
+        <div className="h-16 mt-2 w-28 md:w-44 cursor-pointer flex-shrink-0 hidden sm:inline">
+          <Link href="/">
+            <Image
+              alt="THirdweb ebaylogo"
+              width={100}
+              height={100}
+              src="/ebay-logo.png"
+              className=" h-full w-full object-contain"
+            />
+          </Link>
+        </div>
+        <button className="hidden lg:flex items-center space-x-2 w-20">
+          <p className="text-gray-600 text-sm">Shop by Category</p>
+          <ExpandMoreIcon />
+        </button>
+
+        <div className="flex items-center space-x-2 px-2 md:px-5 py-2 border-black border-2 flex-1">
+          <SearchOutlinedIcon className="text-gray-300 " />
+          <input
+            className="flex-1 outline-none"
+            type="text"
+            placeholder="Search for anything"
+          />
+          <button className="border-gray-300 border-l pl-4 whitespace-nowrap text-sm">
+            {" "}
+            All Catergories <ExpandMoreIcon />
+          </button>
+        </div>
+
+        <button className="hidden sm:inline bg-blue-600 text-white px-5 md:px-10 py-2 border-2 border-blue-700">
+          Search
+        </button>
+        {address ? (
+          <Link href="/create">
+            <button className=" border-2 border-blue-600 px-5 md:px-10 py-2 text-blue-600 hover:bg-blue-600/50 hover:text-white whitespace-nowrap">
+              List Item
+            </button>
+          </Link>
+        ) : (
+          ""
+        )}
+      </section>
+      <hr />
+
+      <section>
+        <div className="flex justify-around items-center py-1 text-sm whitespace-nowrap">
+          <Link className="link" href="/">
+            <p>Home</p>
+          </Link>
+          <p className="link">
+            <FavoriteOutlinedIcon /> Saved{" "}
+          </p>
+          <p className="link">Electronics</p>
+          <p className="link">Fashion</p>
+          <p className="link hidden sm:inline">Health & Beauty</p>
+          <p className="link hidden sm:inline">Motors</p>
+          <p className="link hidden md:inline">Collectibles and Art</p>
+          <p className="link hidden lg:inline">Industrial equipment</p>
+          <p className="link hidden lg:inline">Sports</p>
+          <p className="link hidden lg:inline"> Home & Garden</p>
+          <p className="link hidden xl:inline">Deals</p>
+          <p className="link hidden xl:inline">Sell</p>
+          <p className="link">
+            More <ExpandMoreIcon />
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
